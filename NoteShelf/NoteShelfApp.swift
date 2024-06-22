a6a6f39e42ec0def3a6932f4dedf2c5ee02bf696
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-06-22 15:36:59 +0900

    Initial Commit

---
+++ NoteShelf/NoteShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteShelfApp.swift
+//  NoteShelf
+//
+//  Created by 小坂部泰成 on 2024/06/22.
+//
+
+import SwiftUI
+
+@main
+struct NoteShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
